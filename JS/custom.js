da81a4878f5afbb87fc3d3af244bb1821f41130a
Logan Barsell <loganjbars@gmail.com>
2017-01-17 10:10:49 +0100

    first files added

---
+++ JS/custom.js
@@ -0,0 +1,13 @@
+$('#firstQuestion').click(function() {
+  $('#firstAnswer').toggle(900);
+});
+$('#secondQuestion').click(function() {
+  $('#secondAnswer').toggle(900);
+});  
+
+$('#readMore1').click(function() {
+  $('#more1').toggle(900);
+});
+$('#readMore2').click(function() {
+  $('#more2').toggle(900);
+});
